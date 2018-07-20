a887caa17072d69878e098cf3b0f62483c749255
denis.kalenga <engineereldorado@gmail.com>
2018-07-20 20:11:25 +0300

    bug fixes

--- src/app/constants.service.ts
+++ src/app/constants.service.ts
@@ -7,7 +7,7 @@ export class ConstantsService {
 
   constructor() { }
 
-  public BACKEND_URL="https://zaraka.herokuapp.com/"
+  public BACKEND_URL="https://zaraka.herokuapp.com"
   public FRONTEND_URL="https://zaraportal.herokuapp.com";
 
   // public BACKEND_URL="http://192.168.100.27:5050"
