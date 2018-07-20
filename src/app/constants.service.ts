6c6cf4628ee80d407d001725a2a877cd3b4f89c7
denis.kalenga <engineereldorado@gmail.com>
2018-07-21 00:03:46 +0300

    restrict users roles

--- src/app/constants.service.ts
+++ src/app/constants.service.ts
@@ -7,9 +7,9 @@ export class ConstantsService {
 
   constructor() { }
 
-  // public BACKEND_URL="https://zaraka.herokuapp.com"
-  // public FRONTEND_URL="https://zaraportal.herokuapp.com";
+  public BACKEND_URL="https://zaraka.herokuapp.com"
+  public FRONTEND_URL="https://zaraportal.herokuapp.com";
 
-  public BACKEND_URL="http://192.168.100.27:5050"
-  public FRONTEND_URL="http://localhost:4200"
+  // public BACKEND_URL="http://192.168.100.27:5050"
+  // public FRONTEND_URL="http://localhost:4200"
 }
